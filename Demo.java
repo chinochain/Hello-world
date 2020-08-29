e207fb02618079e85e237402b124e48797ba26c1
chinochain <56431734+chinochain@users.noreply.github.com>
2020-08-29 15:34:31 +0800

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,21 @@
+package com.zyq.test;
+
+/**
+ * @author Administrator
+ * @date 2020/8/29 13:00:35
+ * @description
+ */
+public class Demo {
+    public static void main(String[] args) {
+        int a=0;
+        for(int i=0;i<10;i++){
+            a++;
+        }
+        test(a);
+    }
+
+    public static void test(int a){
+        System.out.println(a);
+
+    }
+}
